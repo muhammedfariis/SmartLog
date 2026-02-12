63cc302dc92fc6c9ce15a2418a03bd01ba2f0732
muhammedfariis <7591faris@gmail.com>
2026-02-12 12:17:53 +0530

    add

--- Frontend/src/common/landingNav.jsx
+++ Frontend/src/common/landingNav.jsx
@@ -13,19 +13,17 @@ const LandingNav = () => {
   return (
     <nav className="w-full flex items-center justify-between px-8 py-3 border-b-2 border-gray-800 shadow-md bg-black/90 backdrop-blur-md">
       
-      {/* Left: Logo + Animated Gradient Text */}
       <div className="flex items-center -my-2 gap-3">
         <img
           src="/images/logosmartlog-removebg-preview.png"
           alt="Logo"
           className="h-20 w-20 object-contain"
         />
-        <span className="text-4xl font-bold bg-gradient-to-r from-yellow-300 via-yellow-500 to-yellow-600 bg-clip-text text-transparent animate-gradient-x">
+        <span className="text-4xl font-bold bg-linear-to-r from-yellow-300/5 via-yellow-500 to-yellow-600/5 bg-clip-text text-transparent animate-gradient-x">
           SMARTLOG
         </span>
       </div>
 
-      {/* Center: Links */}
       <div className="hidden lg:flex gap-4">
         {navLinks.map((link) => (
           <Link
@@ -38,7 +36,6 @@ const LandingNav = () => {
         ))}
       </div>
 
-      {/* Right: Switch + Button */}
       <div className="flex items-center gap-5">
         <Switch />
         <Buttons name="GetReady" Icon={LogIn} />
